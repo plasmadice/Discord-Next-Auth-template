0d5cbd20aef6ebae46730b75983400f4b944d687
plasmadice <19928793+plasmadice@users.noreply.github.com>
2023-06-19 17:37:01 -0400

    Initial commit

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,42 +1,97 @@
 import NextAuth, { NextAuthOptions } from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import FacebookProvider from "next-auth/providers/facebook"
-import GithubProvider from "next-auth/providers/github"
-import TwitterProvider from "next-auth/providers/twitter"
-import Auth0Provider from "next-auth/providers/auth0"
+import DiscordProvider from "next-auth/providers/discord"
 
-// For more information on each option (and a full list of options) go to
-// https://next-auth.js.org/configuration/options
+// https://discord.com/developers/docs/topics/oauth2#shared-resources-oauth2-scopes
+const scopes = ["identify", "guilds"].join(" ")
+
 export const authOptions: NextAuthOptions = {
-  // https://next-auth.js.org/configuration/providers/oauth
+  session: {
+    strategy: "jwt",
+  },
+  
   providers: [
-    Auth0Provider({
-      clientId: process.env.AUTH0_ID,
-      clientSecret: process.env.AUTH0_SECRET,
-      issuer: process.env.AUTH0_ISSUER,
-    }),
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_ID,
-      clientSecret: process.env.FACEBOOK_SECRET,
-    }),
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-    }),
-    TwitterProvider({
-      clientId: process.env.TWITTER_ID,
-      clientSecret: process.env.TWITTER_SECRET,
-      version: "2.0",
+    DiscordProvider({
+      clientId: String(process.env.DISCORD_CLIENT_ID),
+      clientSecret: String(process.env.DISCORD_CLIENT_SECRET),
+      authorization: {
+        params: { scope: scopes.concat(" guilds.members.read") },
+      },
+
+      async profile(profile, tokens) {
+        let isAuthorized = false
+        let isAdult = false
+
+        // Fetch the list of servers the user is a member of
+        const response = await fetch(
+          "https://discord.com/api/users/@me/guilds",
+          {
+            headers: {
+              Authorization: `Bearer ${tokens.access_token}`,
+            },
+          }
+        )
+
+        const guilds = await response.json()
+
+        // Check if the user is a member of the target server
+        const targetGuild = guilds.find(
+          (guild: any) => guild.id === process.env.DISCORD_SERVER_ID
+        )
+
+        if (targetGuild) {
+          // If the user is a member of the target server, they are authorized
+          isAuthorized = true
+
+          // Fetch the member data from our auth server
+          const memberResponse = await fetch(
+            `https://discord.com/api/users/@me/guilds/${process.env.DISCORD_SERVER_ID}/member`,
+            {
+              headers: {
+                Authorization: `Bearer ${tokens.access_token}`,
+              },
+            }
+          )
+
+          const memberData = await memberResponse.json()
+
+          // Check if the member has the 'adult' role
+          isAdult =
+            isAuthorized &&
+            memberData.roles.includes(process.env.DISCORD_ADULT_ROLE)
+
+          // Assign roles to profile
+        }
+
+        return {
+          id: profile.id,
+          name: profile.username,
+          email: profile.email,
+          image: `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.png`,
+          isAuthorized: isAuthorized,
+          isAdult: isAdult,
+        }
+      },
     }),
   ],
+
   callbacks: {
-    async jwt({ token }) {
-      token.userRole = "admin"
-      return token
+    async signIn({ user, account, profile }: any) {
+      if (account.provider === "discord") {
+
+        // Grant access only if the member has the required role
+        return user.isAuthorized
+      }
+
+      // Allow sign-in for other providers
+      return true
+    },
+    
+    async jwt({ token, user }: any) {
+      return { ...token, ...user}
+    },
+    async session({ session, user, token }: any) {
+      session.isAdult = token?.isAdult
+      return session
     },
   },
 }
